223c9ac31f05802fda3f5687a37d8e68e0325dad
Timofey Barmin <tbarmin@machinezone.com>
2015-06-17 17:42:22 +0600

    fixed server allocate_hosts unit test

--- server/test/mzb_api_bench_test.erl
+++ server/test/mzb_api_bench_test.erl
@@ -6,15 +6,16 @@ cloud_plugin_test() ->
     ok = meck:new(dummy_plugin, [non_strict]),
     try
         ok = meck:expect(dummy_plugin, create_cluster, fun ("purpose", 2, ConfigMap) ->
-                                                               ?assertEqual(#{user => "user",
-                                                                              description => "desc",
-                                                                              exclusive_node_usage => false
+                                                               ?assertMatch(#{user := "user",
+                                                                              description := _,
+                                                                              exclusive_node_usage := false
                                                                              }, ConfigMap),
                                                                {ok, dummy_cluster, dummy_user, dummy_hosts}
                                                        end),
         ok = meck:expect(dummy_plugin, destroy_cluster, fun (dummy_cluster) -> ok end),
         ok = meck:expect(dummy_plugin, foo, fun (clusterId) -> ok end),
-        {dummy_hosts, dummy_user, Deallocator} = mzb_api_bench:allocate_hosts("user", "purpose", 1, "desc", false, logger),
+        Config = #{initial_user => "user", purpose => "purpose", nodes_arg => 1, exclusive_node_usage => false},
+        {dummy_hosts, dummy_user, Deallocator} = mzb_api_bench:allocate_hosts(Config, logger),
         ok = Deallocator(),
         ?assert(meck:called(dummy_plugin, create_cluster, '_')),
         ?assert(meck:called(dummy_plugin, destroy_cluster, '_')),
@@ -25,8 +26,9 @@ cloud_plugin_test() ->
 
 
 allocate_existing_hosts_test() ->
-    ?assertError({different_users_for_hosts, ["a", "d"]}, mzb_api_bench:allocate_hosts(nothing, nothing, ["a@b,d@c"], nothing, nothing, logger)),
-    ?assertMatch({["b", "c"], "a", _}, mzb_api_bench:allocate_hosts(nothing, nothing, ["a@b,c"], nothing, nothing, logger)),
-    ?assertMatch({["b", "c"], "a", _}, mzb_api_bench:allocate_hosts(nothing, nothing, ["a@b,a@c"], nothing, nothing, logger)),
-    ?assertMatch({["b", "c", "d"], "a", _}, mzb_api_bench:allocate_hosts(nothing, nothing, ["a@b,a@c,d"], nothing, nothing, logger)),
-    ?assertMatch({["b", "c"], "root", _}, mzb_api_bench:allocate_hosts(nothing, nothing, ["b,c"], nothing, nothing, logger)).
+    Config = #{initial_user => nothing, purpose => nothing, nodes_arg => [], exclusive_node_usage => false},
+    ?assertError({different_users_for_hosts, ["a", "d"]}, mzb_api_bench:allocate_hosts(Config#{nodes_arg => ["a@b,d@c"]}, logger)),
+    ?assertMatch({["b", "c"], "a", _}, mzb_api_bench:allocate_hosts(Config#{nodes_arg => ["a@b,c"]}, logger)),
+    ?assertMatch({["b", "c"], "a", _}, mzb_api_bench:allocate_hosts(Config#{nodes_arg => ["a@b,a@c"]}, logger)),
+    ?assertMatch({["b", "c", "d"], "a", _}, mzb_api_bench:allocate_hosts(Config#{nodes_arg => ["a@b,a@c,d"]}, logger)),
+    ?assertMatch({["b", "c"], "root", _}, mzb_api_bench:allocate_hosts(Config#{nodes_arg => ["b,c"]}, logger)).
