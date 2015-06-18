f7e875e9e5cac33fb128e54847cbba61453dc940
divanov <divanov@machinezone.com>
2015-06-18 12:07:19 +0100

    Extract linting script from travis.yml.

---
+++ bin/lint.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Run python linter on mz-bench
+
+Usage:
+    lint.py <MZ_BENCH_PATH>
+
+Options:
+    -h, --help       Show this help
+"""
+
+import os
+import sys
+import docopt
+import glob
+import subprocess
+from distutils.spawn import find_executable
+
+sys.dont_write_bytecode = True
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+
+    os.environ['PATH'] = ':'.join((
+        os.path.join(os.environ['HOME'], '.local/bin'),
+        os.environ['PATH']))
+    pyflakes = find_executable('pyflakes')
+
+    root = args['<MZ_BENCH_PATH>'] or os.curdir
+
+    python_files = [os.path.join(root, 'bin/mzbench')] + glob.glob(root + '/**/*.py')
+    sys.exit(subprocess.call([pyflakes] + python_files))
