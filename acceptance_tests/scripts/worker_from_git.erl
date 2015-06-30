4637e4e9c47d194abfcb65bb93422ac0a544d7cb
Timofey Barmin <tbarmin@machinezone.com>
2015-06-30 11:56:50 +0600

    Do not use vars in make_install
    because it is not supported yet

--- acceptance_tests/scripts/worker_from_git.erl
+++ acceptance_tests/scripts/worker_from_git.erl
@@ -1,7 +1,6 @@
 [ 
   {make_install, [
     {git, "https://github.com/machinezone/mzbench.git"},
-    {branch, {var, "worker_branch", "master"}},
     {dir, "workers/exec"}]},
   {pool, [{size, 2}, {worker_type, exec_worker}],
     [{execute, "sleep 5"}]}
