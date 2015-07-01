f57e91ec653370520ed0e570e23ac4dbc3b24546
Andrey Vasenin <avasenin@machinezone.com>
2015-07-01 14:53:45 +0000

    Fix sqrt negative values when loop start rps > end rps
    Revalidate formula. Add some comments.
    Remove +1 from IterationNumber to force first loop iteration

--- acceptance_tests/scripts/loop_rate.erl
+++ acceptance_tests/scripts/loop_rate.erl
@@ -2,13 +2,17 @@
           {worker_type, dummy_worker}],
 
     [{loop, [{time, {200, ms}}, {rate, {1.1, rps}}],
-         [{print, "FOO"}]},
+         [{print, "loop#1"}]},
      {loop, [{time, {0.2, sec}}, {rate, {1, rpm}}],
-         [{print, "FOO"}]},
+         [{print, "loop#2"}]},
      {loop, [{time, {0.0033, min}}, {rate, {1.1, rph}}],
-         [{print, "FOO"}]},
+         [{print, "loop#3"}]},
      {loop, [{time, {0.00005, h}},
              {rate, {ramp, linear, {1.1, rpm},
                                    {2.1, rps}}}],
-         [{print, "FOO"}]}]
+         [{print, "loop#4"}]},
+     {loop, [{time, {0.00005, h}},
+             {rate, {ramp, linear, {5, rps},
+                                   {5, rpm}}}],
+         [{print, "loop#5"}]}]
 }].
