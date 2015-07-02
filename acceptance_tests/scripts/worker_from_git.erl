dbb1d6f215c39ae20ce4f24ecf9421589647d651
divanov <divanov@machinezone.com>
2015-07-02 10:21:54 +0100

    Make branch in worker_from_git.erl customizable too

--- acceptance_tests/scripts/worker_from_git.erl
+++ acceptance_tests/scripts/worker_from_git.erl
@@ -1,6 +1,7 @@
 [ 
   {make_install, [
     {git, {var, "mzbench_repo"}},
+    {branch, {var, "mzbench_branch"}},
     {dir, "workers/exec"}]},
   {pool, [{size, 2}, {worker_type, exec_worker}],
     [{execute, "sleep 5"}]}
