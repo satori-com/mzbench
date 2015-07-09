cdfde69f274111c39b9ef74f2e0c4606b19a46da
divanov <divanov@machinezone.com>
2015-07-09 11:23:03 +0100

    Ability to report metrics from lua workers

---
+++ workers/lua/example.erl
@@ -0,0 +1,9 @@
+[ {make_install,
+    [ {git, "file:///home/divanov/mz-bench"},
+      {branch, "lua-metrics"},
+      {dir, "workers/lua"}]},
+  {pool,
+    [{size, 3},
+         {worker_type, lua, lua}],
+    [{main}]}
+].
