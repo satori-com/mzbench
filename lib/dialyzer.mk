2c2957d3402d6262fdd2d8bde7b710731df15318
divanov <divanov@machinezone.com>
2015-07-16 07:40:46 +0100

    Fewer makefile includes

--- lib/dialyzer.mk
+++ lib/dialyzer.mk
@@ -1,14 +1,15 @@
 
 DIALYZABLE_EBINS ?= ebin
+DEPS_PLT ?= deps.plt
 
-# lol
--include ../lib/otp-plt.mk
--include ../../lib/otp-plt.mk
+
+$(HOME)/.otp.plt:
+	dialyzer --output_plt $@ --build_plt --apps erts stdlib kernel crypto syntax_tools os_mon inets
 
 deps.plt: .make/deps-compiled
 	- dialyzer --output_plt $@ --build_plt $(BUILD_PLT_FLAGS) -pa deps/*/ebin
 
-dialyzer.log: deps.plt $(HOME)/.otp.plt .make/compilation-up-to-date
+dialyzer.log: $(DEPS_PLT) $(HOME)/.otp.plt .make/compilation-up-to-date
 	- dialyzer -nn \
 		-o dialyzer.log \
 		-Wno_undefined_callbacks \
@@ -16,7 +17,7 @@ dialyzer.log: deps.plt $(HOME)/.otp.plt .make/compilation-up-to-date
 		-Werror_handling \
 		-Wrace_conditions \
 		--no_check_plt \
-		--plts $< $(HOME)/.otp.plt -- \
+		--plts $(DEPS_PLT) $(HOME)/.otp.plt -- \
 		$(DIALYZABLE_EBINS)
 	-@perl -ne 'print if not /lager_not_running/' -i dialyzer.log
 	-@perl -ne "print if not /The pattern {'error', UtilFailedReason} can never match the type float()/" -i dialyzer.log
--- lib/otp-plt.mk
+++
@@ -1,3 +0,0 @@
-
-$(HOME)/.otp.plt:
-	dialyzer --output_plt $@ --build_plt --apps erts stdlib kernel crypto syntax_tools os_mon inets
