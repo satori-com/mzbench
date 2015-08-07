9eda71fb3af88cdfaa5f2203affc5831bb8996ea
Andrey Vasenin <avasenin@machinezone.com>
2015-08-07 13:41:07 +0000

    Workaround for vsn bug
    Our version is resolved as 0.3.5-25-g4874828 which is valid rev. Right now we take only 7 symbols from this seq and receive incorrect version

--- common_apps/mzbench_utils/src/mzb_git.erl
+++ common_apps/mzbench_utils/src/mzb_git.erl
@@ -1,15 +1,12 @@
 -module(mzb_git).
 
 -export([
-    get_git_sha1/3,
     get_git_short_sha1/3
 ]).
 
-get_git_sha1(GitRepo, GitRef, Logger) ->
+get_git_short_sha1(GitRepo, GitRef, Logger) ->
     case string:tokens(lists:flatten(mzb_subprocess:exec_format("git ls-remote ~s ~s", [GitRepo, GitRef], [stderr_to_stdout], Logger)), "\t") of
-        [Commit | _] -> Commit;
+        [Commit | _] -> lists:sublist(Commit, 7);
         _ -> GitRef
     end.
 
-get_git_short_sha1(GitRepo, GitRef, Logger) ->
-    lists:sublist(get_git_sha1(GitRepo, GitRef, Logger), 7).
