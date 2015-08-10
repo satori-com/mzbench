a0a3f48f884d76d115f2c91d30f4b7846e97635d
Andrey Vasenin <avasenin@machinezone.com>
2015-08-10 14:43:41 +0000

    fix issue after rebase

--- acceptance_tests/scripts/python_worker_from_git.erl
+++ acceptance_tests/scripts/python_worker_from_git.erl
@@ -2,7 +2,7 @@
     {make_install, [
       {git, {var, "mzbench_repo"}},
       {branch, {var, "worker_branch"}},
-      {dir, "worker-templates/python_empty"}]},
+      {dir, "worker_templates/python_empty"}]},
     {pool, [
         {size, 3},
         {worker_type, python_empty, python}
