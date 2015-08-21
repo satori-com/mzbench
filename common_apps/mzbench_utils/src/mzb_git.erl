b136dae822d7bb050081ba47ddb3070b379b4829
Timofey Barmin <tbarmin@machinezone.com>
2015-08-21 22:14:08 +0600

    Do 'git unshallow' in case of local repo

--- common_apps/mzbench_utils/src/mzb_git.erl
+++ common_apps/mzbench_utils/src/mzb_git.erl
@@ -5,7 +5,14 @@
 ]).
 
 get_git_short_sha1(GitRepo, GitRef, Logger) ->
-    case string:tokens(lists:flatten(mzb_subprocess:exec_format("git ls-remote ~s ~s", [GitRepo, GitRef], [stderr_to_stdout], Logger)), "\t") of
+    case filelib:is_dir(GitRepo) of
+        true ->
+            _ = (catch mzb_subprocess:exec_format("bash -c \"cd ~s && git fetch --unshallow\"", [GitRepo], [stderr_to_stdout], Logger));
+        false ->
+            ok
+    end,
+    Output = mzb_subprocess:exec_format("git ls-remote ~s ~s", [GitRepo, GitRef], [], Logger),
+    case string:tokens(lists:flatten(Output), "\t") of
         [Commit | _] -> lists:sublist(Commit, 7);
         _ -> GitRef
     end.
