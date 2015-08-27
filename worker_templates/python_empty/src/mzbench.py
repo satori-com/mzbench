fd265f4106292ac2274b3c3a8719f1fc0bc8057f
Timofey Barmin <tbarmin@machinezone.com>
2015-08-27 13:14:12 +0600

    Interpret results of python worker functions

--- worker_templates/python_empty/src/mzbench.py
+++ worker_templates/python_empty/src/mzbench.py
@@ -2,27 +2,50 @@ import os
 import sys
 
 # Exported functions
-def define_metrics(metrics_list):
-    _mzbench_pipe.write("D {0}.\n".format(_encode_metrics_list(metrics_list)))
-
 
 def notify(metric, value):
     _mzbench_pipe.write("M {{{0}, {1}}}.\n".format(_encode_metric(metric), value))
 
 
 # Internal functions
-def _instruction_end():
-    _mzbench_pipe.write("T\n")
+def _instruction_end(result):
+    _mzbench_pipe.write("T {0}.\n".format(_encode_term(result)))
 
 
-def _instruction_failed():
-    _mzbench_pipe.write("E\n")
+def _instruction_failed((t, o, st)):
+    _mzbench_pipe.write("E {0} {1}.\n".format(t, o))
 
 
 def _module_funcs(module_name):
-    FuncList = dir(module_name)
-    _mzbench_pipe.write("F {0}.\n".format(_encode_funcs_list(FuncList)))
+    return dir(module_name)
 
+    #_mzbench_pipe.write("F {0}.\n".format(_encode_term(FuncList)))
+
+
+def _encode_term(term):
+    T = type(term)
+    if   (list == T): return _encode_list(term)
+    elif (tuple == T): return _encode_tuple(term)
+    elif (int == T): return _encode_num(term)
+    elif (float == T): return _encode_num(term)
+    elif (str == T): return _encode_str(term)
+    elif (unicode == T): return _encode_str(term)
+    else: return _encode_str("<unknown python term: {0}>".format(term))
+
+
+def _encode_list(l):
+    return '[' + ', '.join([_encode_term(e) for e in l]) + ']'
+
+
+def _encode_tuple(l):
+    return '{' + ', '.join([_encode_term(e) for e in l]) + '}'
+
+
+def _encode_num(n):
+    return str(n)
+
+def _encode_str(s):
+    return '"{0}"'.format(s)
 
 def _encode_funcs_list(func_list):
     return '[' + ', '.join(['"{0}"'.format(e) for e in func_list]) + ']'
@@ -32,15 +55,10 @@ def _encode_metric(metric):
     return '{{"{0}", {1}}}'.format(_encode_string_for_erlang(metric[0]), _encode_string_for_erlang(metric[1]))
 
 
-def _encode_metrics_list(metrics_list):
-    if isinstance(metrics_list, list):
-        return '[' + ', '.join(map(_encode_metrics_list, metrics_list)) + ']'
-    else:
-        return _encode_metric(metrics_list)
-
 # May fail in some complicated cases
 def _encode_string_for_erlang(string):
     return string.replace('\\', '\\\\').replace('"', '\"')
+
 
 # MZBench communication initialization
 if 'MZ_PYTHON_WORKER_FIFO_NAME' not in os.environ:
