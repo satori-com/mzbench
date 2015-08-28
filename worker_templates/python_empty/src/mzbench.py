aaa5e145649c5032c487527c513adf9336483735
Timofey Barmin <tbarmin@machinezone.com>
2015-08-28 22:30:48 +0600

    Ability to call erl functions from python

--- worker_templates/python_empty/src/mzbench.py
+++ worker_templates/python_empty/src/mzbench.py
@@ -3,8 +3,21 @@ import sys
 
 # Exported functions
 
+class mzb_atom(str):
+    pass
+
+
 def notify(metric, value):
     _mzbench_pipe.write("M {{{0}, {1}}}.\n".format(_encode_metric(metric), value))
+
+
+def get_metric_value(name):
+    return _call(mzb_atom('mzb_metrics'), mzb_atom('get_value'), [name])
+
+
+def _call(module, function, args):
+    _mzbench_pipe.write("C {0}.\n".format(_encode_term((module, function, args))))
+    return _read_call_result()
 
 
 # Internal functions
@@ -18,9 +31,20 @@ def _instruction_failed((t, o, st)):
 
 def _module_funcs(module_name):
     return dir(module_name)
-
     #_mzbench_pipe.write("F {0}.\n".format(_encode_term(FuncList)))
 
+def _read_call_result():
+    res = sys.stdin.readline().rstrip()
+    line_num = int(sys.stdin.readline().rstrip())
+    content = []
+    while line_num > 0:
+        content.append(sys.stdin.readline().rstrip())
+        line_num -= 1
+
+    if (res == "OK"):
+        return eval("\n".join(content)) if content else None
+    else:
+        raise Exception("\n".join(content))
 
 def _encode_term(term):
     T = type(term)
@@ -30,9 +54,14 @@ def _encode_term(term):
     elif (dict == T): return _encode_dict(term)
     elif (int == T): return _encode_num(term)
     elif (float == T): return _encode_num(term)
+    elif (mzb_atom == T): return _encode_atom(term)
     elif (str == T): return _encode_str(term)
     elif (unicode == T): return _encode_str(term)
     else: return _encode_str("<unknown python term: {0}>".format(term))
+
+
+def _encode_atom(a):
+    return "'{0}'".format(a)
 
 
 def _encode_list(l):
