b40f282bfaec716f86251bb182fcdae15373343e
Dmitry Ivanov <divanov@machinezone.com>
2015-09-26 10:35:48 -0400

    Don't call git ls-remote if reference is already a SHA string

--- common_apps/mzbench_utils/src/mzb_git.erl
+++ common_apps/mzbench_utils/src/mzb_git.erl
@@ -11,9 +11,13 @@ get_git_short_sha1(GitRepo, GitRef, Logger) ->
         false ->
             ok
     end,
-    Output = mzb_subprocess:exec_format("git ls-remote ~s ~s", [GitRepo, GitRef], [], Logger),
-    case string:tokens(lists:flatten(Output), "\t") of
-        [Commit | _] -> lists:sublist(Commit, 7);
-        _ -> GitRef
+    case {length(GitRef), lists:all(fun(Char) -> lists:member(Char, "01234567890abcdef") end, GitRef)} of
+        {40, true} -> GitRef;
+        _ ->
+            Output = mzb_subprocess:exec_format("git ls-remote ~s ~s", [GitRepo, GitRef], [], Logger),
+            case string:tokens(lists:flatten(Output), "\t") of
+                [Commit | _] -> lists:sublist(Commit, 7);
+                _ -> GitRef
+            end
     end.
 
