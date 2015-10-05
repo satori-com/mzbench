2e293773a131873aa751ee94c5fa8054943387b1
Dmitry Ivanov <divanov@machinezone.com>
2015-10-05 17:05:35 +0600

    Add http resources

--- acceptance_tests/scripts/unicode_resource.erl
+++ acceptance_tests/scripts/unicode_resource.erl
@@ -1,5 +1,7 @@
 [{include_resource, strings, {var, "strings_filename"}},
+ {include_resource, readme, "https://raw.githubusercontent.com/machinezone/mzbench/master/README.md", text},
  {pool, [{size, 1},
         {worker_type, dummy_worker}],
-    [{print, {choose, {resource, strings}}}]
+    [{print, {choose, {resource, strings}}},
+     {print, {resource, readme}}]
 }].
