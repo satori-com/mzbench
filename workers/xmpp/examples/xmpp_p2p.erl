cb3727276125a934438351702ec1341e771b89ad
Andrey Vasenin <avasenin@machinezone.com>
2015-10-07 11:34:19 +0000

    Use uniform from random module. crypto:random_uniform is slow and affect benches with big number of workers

--- workers/xmpp/examples/xmpp_p2p.erl
+++ workers/xmpp/examples/xmpp_p2p.erl
@@ -30,7 +30,7 @@
             {loop, [{time, {{numvar, "duration", 5}, min}},
                     {rate, {ramp, linear, {{numvar, "min-msgs-rps", 1}, rps},
                                           {{numvar, "max-msgs-rps", 10}, rps}}}],
-                [{send_message, {recepient, "user", 1, {numvar, "users-number", 1000}}, {marker, 50}, skip}]
+                [{send_message, {recepient, "user", {numvar, "users-number", 1000}}, {marker, 50}, skip}]
             },
 
             % close socket and halt stream parser
