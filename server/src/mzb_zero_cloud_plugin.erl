9e8f747374c60013aeebcf1646f5df6020a7d260
Timofey Barmin <tbarmin@machinezone.com>
2015-10-22 13:49:14 +0600

    Add zero cloud plugin

---
+++ server/src/mzb_zero_cloud_plugin.erl
@@ -0,0 +1,21 @@
+-module(mzb_zero_cloud_plugin).
+
+-export([
+    start/2,
+    create_cluster/3,
+    destroy_cluster/1
+]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start(Name, Opts) -> {Name, Opts}.
+
+create_cluster({_Name, _Opts}, _N, _Config) ->
+    {ok, _Ref = erlang:make_ref(), _User = undefined, ["localhost"]}.
+
+destroy_cluster(_Ref) ->
+    ok.
+
+
