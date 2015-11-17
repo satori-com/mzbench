54f594c3d25f0409a9bbdef52fadef07321187dc
Timofey Barmin <tbarmin@machinezone.com>
2015-11-17 19:11:29 +0600

    Temporarily commented out workers.<node>.<status> graphs

--- node/apps/mzbench/src/mzb_worker_runner.erl
+++ node/apps/mzbench/src/mzb_worker_runner.erl
@@ -7,25 +7,25 @@
 -spec run_worker_script([script_expr()], worker_env() , module(), Pool :: pid(), PoolName ::string())
     -> ok.
 run_worker_script(Script, Env, {WorkerProvider, Worker}, PoolPid, PoolName) ->
-    NodeName = mzb_utility:hostname_str(node()),
+    %NodeName = mzb_utility:hostname_str(node()),
     Res =
         try
             _ = random:seed(now()),
             ok = mzb_metrics:notify(mzb_string:format("workers.~s.started", [PoolName]), 1),
-            ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.started", [PoolName, NodeName]), 1),
+            %ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.started", [PoolName, NodeName]), 1),
 
             InitialState = WorkerProvider:init(Worker),
             {WorkerResult, WorkerResultState} = mzbl_interpreter:eval(Script, InitialState, Env, WorkerProvider),
             ok = mzb_metrics:notify(mzb_string:format("workers.~s.ended", [PoolName]), 1),
-            ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.ended", [PoolName, NodeName]), 1),
+            %ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.ended", [PoolName, NodeName]), 1),
             _ = (catch  WorkerProvider:terminate(WorkerResult, WorkerResultState)),
             {ok, WorkerResult}
         catch
             C:E ->
                 ok = mzb_metrics:notify(mzb_string:format("workers.~s.ended", [PoolName]), 1),
-                ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.ended", [PoolName, NodeName]), 1),
+                %ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.ended", [PoolName, NodeName]), 1),
                 ok = mzb_metrics:notify(mzb_string:format("workers.~s.failed", [PoolName]), 1),
-                ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.failed", [PoolName, NodeName]), 1),
+                %ok = mzb_metrics:notify(mzb_string:format("workers.~s.~s.failed", [PoolName, NodeName]), 1),
                 _ = (catch WorkerProvider:terminate({C, E, erlang:get_stacktrace()}, undefined)),
                 {exception, node(), {C, E, erlang:get_stacktrace()}}
         end,
