c2a781c3f44da2157742e5351e67c82bd3e9c11f
Vadim Litvinov <vlitvinov@machinezone.com>
2016-01-21 17:23:24 +0600

    The user can exit from fullscreen graph by pressing "Esc" or "Enter"

--- server/dashboard/js/components/Modal.react.js
+++ server/dashboard/js/components/Modal.react.js
@@ -2,7 +2,7 @@ import React, { findDOMNode } from 'react';
 
 class Modal extends React.Component {
     componentDidMount() {
-        $(findDOMNode(this.refs.modal)).modal({backdrop: "static", keyboard: true, show: false});
+        $(findDOMNode(this.refs.modal)).modal({backdrop: "static", show: false});
     }
 
     componentWillUnmount() {
@@ -17,46 +17,24 @@ class Modal extends React.Component {
         $(findDOMNode(this.refs.modal)).modal("hide");
     }
 
-    renderHeader() {
-        if(this.props.render_title) {
-            return (
-                <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 className="modal-title">{this.props.title}</h4>
-                </div>
-            );
-        } else {
-            return;
-        }
-    }
-
-    renderFooter() {
-        if(this.props.render_submit_button) {
-            return (
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={this.props.onOk}>Submit</button>
-                </div>
-            );
-        } else {
-            return (
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-danger" onClick={this.props.onOk}>Close</button>
-                </div>
-            );
-        }
-    }
-
     render() {
         return (
             <div ref="modal" className="modal fade">
-                <div className={this.props.render_fullscreen?"modal-dialog fullscreen-modal-dialog":"modal-dialog"}>
+                <div className="modal-dialog">
                     <div className="modal-content">
-                        {this.renderHeader()}
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 className="modal-title">{this.props.title}</h4>
+                        </div>
+                        
                         <div className="modal-body">
                             {this.props.children}
                         </div>
-                        {this.renderFooter()}
+                        
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={this.props.onOk}>Submit</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -66,16 +44,11 @@ class Modal extends React.Component {
 
 Modal.propTypes = {
     title: React.PropTypes.string,
-    render_fullscreen: React.PropTypes.bool,
-    render_title: React.PropTypes.bool,
-    render_submit_button: React.PropTypes.bool
+    onOk: React.PropTypes.func.isRequired
 };
 
 Modal.defaultProps = {
-    title: "",
-    render_fullscreen: false,
-    render_title: true,
-    render_submit_button: true
+    title: ""
 };
 
 export default Modal;
