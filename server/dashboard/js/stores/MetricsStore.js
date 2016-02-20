e3d370dcea2b66b44b0f09ece49309dd10a6cf8b
Vadim Litvinov <vlitvinov@machinezone.com>
2016-02-20 16:18:07 +0600

    Each metric is streamed by a separate stream with unique ID

--- server/dashboard/js/stores/MetricsStore.js
+++ server/dashboard/js/stores/MetricsStore.js
@@ -2,47 +2,25 @@ import { EventEmitter } from 'events';
 import Dispatcher from '../dispatcher/AppDispatcher';
 import ActionTypes from '../constants/ActionTypes';
 import MZBenchActions from '../actions/MZBenchActions';
-import Misc from '../utils/Misc.js';
 
 const CHANGE_EVENT = 'metrics_change';
-const DATA_RESAMPLING_RATE = 1; // hours
 
 let data = {
-    benchId: undefined,
-    guid: undefined,
-    
-    last_resampling_date: undefined,
-    
-    starting_date: undefined,
-    map: new Map([]),
-    batch_counter: new Map([])
+    streams: new Map([])
 };
 
-function _clearData() {
-    data.last_resampling_date = Date.now();
-    
-    data.starting_date = undefined;
-    data.map.clear();
-    data.batch_counter.clear();
-}
-
-function _updateData(metric, rawData) {
+function _updateData(streamId, rawData) {
     const updates = rawData.split("\n");
     updates.forEach((update) => {
-        _applyUpdate(metric, update);
+        _applyUpdate(streamId, update);
     });
 }
 
-function _updateBatchCounter(metric) {
-    let m = data.batch_counter;
-    if (m.has(metric)) {
-        m.set(metric, m.get(metric) + 1);
-    } else {
-        m.set(metric, 1);
-    }
+function _updateBatchCounter(streamId) {
+    data.streams.get(streamId).batch_counter = data.streams.get(streamId).batch_counter + 1;
 }
 
-function _applyUpdate(metric, update) {
+function _applyUpdate(streamId, update) {
     const tokens = update.split("\t");
 
     if(tokens.length >= 4) {
@@ -52,33 +30,26 @@ function _applyUpdate(metric, update) {
         const max = Number.parseFloat(tokens[3]);
 
         if(!Number.isNaN(date) && !Number.isNaN(value) && !Number.isNaN(min) && !Number.isNaN(max)) {
-            _addObservation(metric, { date: date, value: value, min: min, max: max });
+            _addObservation(streamId, { date: date, value: value, min: min, max: max });
         }
     }
 }
 
-function _addObservation(metric, observation) {
-    if(data.map.has(metric)) {
-        _updateMetric(metric, observation);
-    } else {
-        _createMetric(metric, observation);
+function _addObservation(streamId, observation) {
+    if(!data.streams.get(streamId).starting_date) {
+        data.streams.get(streamId).starting_date = observation.date;
     }
+    
+    data.streams.get(streamId).data.push({
+        "date": _convertDate(streamId, observation.date), 
+        "value": observation.value, 
+        "min": observation.min, 
+        "max": observation.max
+    });
 }
 
-function _createMetric(metric, observation) {
-    if(!data.starting_date) {
-        data.starting_date = observation.date;
-    }
-
-    data.map.set(metric, new Array({"date": _convertDate(observation.date), "value": observation.value, "min": observation.min, "max": observation.max}));
-}
-
-function _updateMetric(metric, observation) {
-    data.map.get(metric).push({"date": _convertDate(observation.date), "value": observation.value, "min": observation.min, "max": observation.max});
-}
-
-function _convertDate(rawDate) {
-    return rawDate - data.starting_date;
+function _convertDate(streamId, rawDate) {
+    return rawDate - data.streams.get(streamId).starting_date;
 }
 
 class MetricsStore extends EventEmitter {
@@ -99,80 +70,60 @@ class MetricsStore extends EventEmitter {
         this.removeListener(CHANGE_EVENT, callback);
     }
 
-    getCurrentBenchId() {
-        return data.benchId;
-    }
-
-    performResamplingIfNeeded() {
-        const sec_from_last_resampling = (Date.now() - data.last_resampling_date)/1000;
-        if(sec_from_last_resampling > DATA_RESAMPLING_RATE*3600) {
-            let metrics_list = [];
-            data.map.forEach((value, key) => {
-                metrics_list.push(key);
-            });
-        
-            this.resetSubscriptions(data.benchId);
-            metrics_list.map((metric) => {
-                this.addSubscription(metric);
-            });
+    updateMetricData(streamId, rawData) {
+        if(data.streams.has(streamId)) {
+            _updateData(streamId, rawData);
         }
     }
 
-    resetSubscriptions(newBenchId) {
-        _clearData();
-        data.benchId = newBenchId;
-        data.guid = Misc.gen_guid();
-    }
-
-    addSubscription(metrics) {
-        MZBenchActions.sendSubscribe(data.benchId, metrics, data.guid);
-    }
-
-    changeCurrentBench(benchId, GUID) {
-        data.benchId = benchId;
-        data.guid = GUID;
-        _clearData();
-    }
-
-    isDataLoaded() {
-        return true;
-    }
-
-    updateMetricData(metric, guid, rawData) {
-        if(data.guid == guid) {
-            _updateData(metric, rawData);
+    updateMetricBatchCounter(streamId) {
+        if(data.streams.has(streamId)) {
+            _updateBatchCounter(streamId);
         }
     }
 
-    updateMetricBatchCounter(metric, guid) {
-        if(data.guid == guid) {
-            _updateBatchCounter(metric);
-            this.performResamplingIfNeeded();
-        }
+    subscribeToMetric(benchId, metric) {
+        const streamId = MZBenchActions.startStream(benchId, metric);
+        data.streams.set(streamId, {
+            starting_date: undefined,
+            batch_counter: 0,
+            data: []
+        });
+        return streamId;
     }
 
-    getMetricData(metric) {
-        if(data.map.has(metric)) {
-            return data.map.get(metric);
+    unsubscribeFromMetric(streamId) {
+        MZBenchActions.stopStream(streamId);
+        data.streams.delete(streamId);
+    }
+
+    getMetricData(streamId) {
+        if(data.streams.has(streamId)) {
+            return data.streams.get(streamId).data;
         } else {
-            return [];
+            return undefined;
         }
     }
 
-    getBatchCounter(metric) {
-        if(data.batch_counter.has(metric)) {
-            return data.batch_counter.get(metric);
+    getBatchCounter(streamId) {
+        if(data.streams.has(streamId)) {
+            return data.streams.get(streamId).batch_counter;
         } else {
-            return 0;
+            return undefined;
         }
     }
 
-    getMetricMaxDate(metric) {
-        if(data.map.has(metric)) {
-            let m = data.map.get(metric);
-            return m[m.length - 1]["date"];
+    getMetricMaxDate(streamId) {
+        if(data.streams.has(streamId)) {
+            const m = data.streams.get(streamId).data;
+            
+            if(m.length > 0) {
+                return m[m.length - 1]["date"];
+            } else {
+                return 0;
+            }
         } else {
-            return 0;
+            return undefined;
         }
     }
 };
@@ -182,15 +133,12 @@ export default _MetricsStore;
 
 _MetricsStore.dispatchToken = Dispatcher.register((action) => {
     switch(action.type) {
-        case ActionTypes.SUBSCRIBE_METRICS:
-            _MetricsStore.addSubscription(action.metrics);
-            break;
         case ActionTypes.METRIC_DATA:
-            _MetricsStore.updateMetricData(action.metric, action.guid, action.data);
+            _MetricsStore.updateMetricData(action.stream_id, action.data);
             _MetricsStore.emitChange();
             break;
         case ActionTypes.METRIC_BATCH_END:
-            _MetricsStore.updateMetricBatchCounter(action.metric, action.guid);
+            _MetricsStore.updateMetricBatchCounter(action.stream_id);
             _MetricsStore.emitChange();
             break;
         default:
