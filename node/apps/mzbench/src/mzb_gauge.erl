135294c0558507746b6b76f8fc7b32a0e9fd5272
Timofey Barmin <tbarmin@machinezone.com>
2016-04-12 16:32:22 +0600

    Delete gauge value after use

--- node/apps/mzbench/src/mzb_gauge.erl
+++ node/apps/mzbench/src/mzb_gauge.erl
@@ -7,7 +7,8 @@
     start_link/0,
     create/1,
     notify/2,
-    get_value/1
+    get_value/1,
+    take_value/1
 ]).
 
 %% gen_server callbacks
@@ -26,15 +27,35 @@ start_link() ->
 create(Name) ->
     notify(Name, 0).
 
+%% In order to support atomic take_value (and since there is no support for
+%% ets:take/2 in R17 yet) we have to do notify and take_value synchronously.
+%% Gauges are not suppposed to be notified very often so this should work
 notify(Name, Value) ->
-    ets:insert(?MODULE, {Name, Value}),
-    ok.
+    gen_server:call(?MODULE, {notify, Name, Value}, infinity).
+
+take_value(Name) ->
+    case gen_server:call(?MODULE, {take_value, Name}, infinity) of
+        {ok, V} -> V;
+        not_found -> erlang:error(not_found)
+    end.
 
 get_value(Name) ->
     case ets:lookup(?MODULE, Name) of
         [{_, R}] -> R;
         [] -> erlang:error(not_found)
     end.
+
+% Uncomment when R17 support is dropped (ets:take is unupported in R17)
+%notify(Name, Value) ->
+%    ets:insert(?MODULE, {Name, Value}),
+%    ok.
+%
+%
+%take_value(Name) ->
+%    case ets:take(?MODULE, Name) of
+%        [{_, R}] -> R;
+%        [] -> erlang:error(not_found)
+%    end.
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -43,6 +64,19 @@ get_value(Name) ->
 init([]) ->
     _ = ets:new(?MODULE, [named_table, set, public]),
     {ok, #s{}}.
+
+handle_call({notify, Name, Value}, _From, State) ->
+    ets:insert(?MODULE, {Name, Value}),
+    {reply, ok, State};
+
+handle_call({take_value, Name}, _From, State) ->
+    case ets:lookup(?MODULE, Name) of
+        [{_, R}] ->
+            ets:delete(?MODULE, Name),
+            {reply, {ok, R}, State};
+        [] ->
+            {reply, not_found, State}
+    end;
 
 handle_call(_Request, _From, State) ->
    {noreply, State}.
