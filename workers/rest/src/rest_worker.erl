518ab2d7f00db6dbaf17e8a3739beff146937dcd
John Koenig <john@postmates.com>
2016-05-26 17:19:12 -0500

    Request now selects between multiple bodies

--- workers/rest/src/rest_worker.erl
+++ workers/rest/src/rest_worker.erl
@@ -8,10 +8,12 @@
          get/3,
          post/3,
          put/3,
-         request/7
+         request/6
         ]).
 
--export([set_body/4
+-export([set_body/4,
+         load_body_values/1,
+         pick_body/0
         ]).
 
 -type meta() :: [{Key :: atom(), Value :: any()}].
@@ -23,7 +25,6 @@
     { host :: http_host()
     , port :: http_port()
     , options = [] :: http_options()
-    , body :: binary()
     , headers = dict:new() :: dict:dict()
     }).
 
@@ -40,6 +41,7 @@
 
 -spec initial_state() -> state().
 initial_state() ->
+    ets:new(body_table, [private, named_table]),
     #state{}.
 
 -spec metrics() -> list().
@@ -68,29 +70,42 @@ set_options(State, _Meta, NewOptions) ->
     {nil, State#state{options = NewOptions}}.
 
 -spec set_body(state(), meta(), undefined | binary(), term()) -> {nil, state()}.
+%% Binary cases are being kept for backwards compatibility
 set_body(State, _Meta, undefined, Value) when is_binary(Value) ->
-    {nil, State#state{body=Value}};
-set_body(State=#state{headers=Headers}, _Meta, ContentType, Value) when is_binary(ContentType), is_binary(Value) ->
+    set_body(State, _Meta, undefined, [Value]);
+set_body(State, _Meta, ContentType, Value) when is_binary(ContentType), is_binary(Value) ->
+    set_body(State, _Meta, ContentType, [Value]);
+set_body(State, _Meta, undefined, Values) when is_list(Values) ->
+    load_body_values(Values),
+    {nil, State};
+set_body(State=#state{headers=Headers}, _Meta, ContentType, Values) when is_binary(ContentType), is_list(Values) ->
     NewHeaders = dict:store(<<"Content-Type">>, ContentType, Headers),
-    {nil, State#state{headers=NewHeaders, body=Value}}.
+    load_body_values(Values),
+    {nil, State#state{headers=NewHeaders}}.
+
+load_body_values(Values) ->
+    %% We load an ets table with body values for #BlazingSpeed
+    F = fun(Body, Acc) -> true = ets:insert(body_table, {Acc, Body}), Acc + 1 end,
+    lists:foldl(F, 0, Values).
 
 -spec get(state(), meta(), string()) -> {nil, state()}.
-get(#state{host = Host, port = Port, headers = Headers, body = Body, options = Options} = State, _Meta, Endpoint) ->
-    request(get, Host, Port, Endpoint, Headers, Body, Options),
+get(#state{host = Host, port = Port, headers = Headers, options = Options} = State, _Meta, Endpoint) ->
+    request(get, Host, Port, Endpoint, Headers, Options),
     {nil, State}.
 
 -spec post(state(), meta(), string()) -> {nil, state()}.
-post(#state{host = Host, port = Port, headers = Headers, body = Body, options = Options} = State, _Meta, Endpoint) ->
-    request(post, Host, Port, Endpoint, Headers, Body, Options),
+post(#state{host = Host, port = Port, headers = Headers, options = Options} = State, _Meta, Endpoint) ->
+    request(post, Host, Port, Endpoint, Headers, Options),
     {nil, State}.
 
 -spec put(state(), meta(), string()) -> {nil, state()}.
-put(#state{host = Host, port = Port, headers = Headers, body = Body, options = Options} = State, _Meta, Endpoint) ->
-    request(put, Host, Port, Endpoint, Headers, Body, Options),
+put(#state{host = Host, port = Port, headers = Headers, options = Options} = State, _Meta, Endpoint) ->
+    request(put, Host, Port, Endpoint, Headers, Options),
     {nil, State}.
 
-request(Method, Host, Port, Endpoint, Headers, Body, Options) ->
+request(Method, Host, Port, Endpoint, Headers, Options) ->
     URL = lists:flatten(io_lib:format("http://~s:~p~s", [Host, Port, Endpoint])),
+    Body = pick_body(),
     Response = ?TIMED("latency", hackney:request(
         Method, list_to_binary(URL), dict:to_list(Headers), Body, [{follow_redirect, true}] ++ Options)),
     record_response(Response).
@@ -106,4 +121,20 @@ record_response(Response) ->
         E ->
             lager:error("hackney:request failed: ~p", [E]),
             mzb_metrics:notify({"other_fail", counter}, 1)
+    end.
+
+pick(Min, Max) when Min >= 0, Max > Min->
+    (erlang:phash2(process_info(self(), reductions)) rem Max) + Min.
+
+pick_body() ->
+    case ets:info(body_table, size) of
+        undefined ->
+            undefined;
+
+        0 ->
+            undefined;
+        N ->
+            BodyN = pick(0, N),
+            [{BodyN, Body}] = ets:lookup(body_table, BodyN),
+            Body
     end.
