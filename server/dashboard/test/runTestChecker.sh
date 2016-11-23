b8d8d38a5ea33ac9a2c36060d26afb6107d3b4de
Renat Idrisov <ridrisov@mz.com>
2016-11-23 18:19:15 +0100

    Test-related fixes

--- server/dashboard/test/runTestChecker.sh
+++ server/dashboard/test/runTestChecker.sh
@@ -1,3 +1,5 @@
-npm install babel-preset-latest babel-cli
+set -e
+npm install
+npm install babel-cli babel-preset-es2015
 node node_modules/babel-cli/bin/babel.js --presets es2015 js/utils/BenchChecker.js > test/es5Checker.js
 node test/testChecker.js
