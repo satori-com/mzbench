37b4c2a2d2c08c3a238b3c9b745de9aa5597d170
Renat Idrisov <ridrisov@mz.com>
2017-03-23 11:59:40 +0100

    Iterator checker

--- node/apps/dummy_worker/src/dummy_worker.erl
+++ node/apps/dummy_worker/src/dummy_worker.erl
@@ -1,12 +1,12 @@
 -module(dummy_worker).
 
--export([initial_state/0, metrics/0,
+-export([initial_state/0, metrics/0, check_iterator/3,
          print/3, test_method/3, test_pre_hook/1, test_proplist/3, doubled_print_counter/0]).
 
 -type state() :: string().
 
 -spec initial_state() -> state().
-initial_state() -> "".
+initial_state() -> -1.
 
 metrics() ->
     [{"print", counter, #{visibility => false, rps_visibility => true}},
@@ -23,6 +23,11 @@ print(State, _Meta, Text) ->
     Finish = os:timestamp(),
     _ = mzb_metrics:notify({"dummy", histogram}, timer:now_diff(Finish, Start)),
     {nil, State}.
+
+check_iterator(State, _Meta, Iterator) ->
+    if Iterator /= State + 1 -> lager:info("Iterator problem, should be ~p instead of ~p", [State + 1, Iterator]);
+        true -> ok end,
+    {nil, Iterator}.
 
 test_method(State, _Meta, Text) ->
     {nil, Text ++ State}.
