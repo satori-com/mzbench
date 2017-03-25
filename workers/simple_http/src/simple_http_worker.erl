ef231ce630e9a27235cfb22d27ad2f89edc81a5d
Timofey Barmin <tbarmin@mz.com>
2017-03-25 12:10:15 -0700

    Add http post in simple_http worker

--- workers/simple_http/src/simple_http_worker.erl
+++ workers/simple_http/src/simple_http_worker.erl
@@ -1,7 +1,7 @@
 -module(simple_http_worker).
 
 -export([initial_state/0, metrics/0,
-         get/3, get/4, random_get/3]).
+         get/3, get/4, post/5, random_get/3]).
 
 -type state() :: term().
 -type meta() :: [{Key :: atom(), Value :: any()}].
@@ -28,21 +28,21 @@ get(State, _Meta, URL) ->
 
 -spec get(state(), meta(), string(), [{atom(), term()}]) -> {nil, state()}.
 get(State, _Meta, URL, Options) ->
-    StartTime = os:timestamp(),
     ShouldLog = proplists:get_value(log, Options, false),
     ExpectedCode = proplists:get_value(expected, Options, 200),
 
     ShouldLog andalso lager:info("GET ~s", [URL]),
 
     {HackneyOptions, Headers} = parse_options(Options),
+    StartTime = os:timestamp(),
     Response = hackney:request(get, list_to_binary(URL), Headers, <<"">>, HackneyOptions),
+    Latency = timer:now_diff(os:timestamp(), StartTime),
 
     case Response of
         {ok, _, _, BodyRef} -> hackney:skip_body(BodyRef);
         _ -> ok
     end,
 
-    Latency = timer:now_diff(os:timestamp(), StartTime),
     mzb_metrics:notify({"latency", histogram}, Latency),
 
     case Response of
@@ -50,6 +50,36 @@ get(State, _Meta, URL, Options) ->
             mzb_metrics:notify({"http_ok", counter}, 1);
         {ok, _, _, _} = Reply ->
             lager:error("GET failed: ~p", [Reply]),
+            mzb_metrics:notify({"http_fail", counter}, 1);
+        E ->
+            lager:error("hackney:request failed: ~p", [E]),
+            mzb_metrics:notify({"other_fail", counter}, 1)
+    end,
+    {nil, State}.
+
+post(State, _Meta, URL, Body, Options) ->
+    ShouldLog = proplists:get_value(log, Options, false),
+    ExpectedCode = proplists:get_value(expected, Options, 200),
+
+    ShouldLog andalso lager:info("POST ~s", [URL]),
+
+    {HackneyOptions, Headers} = parse_options(Options),
+    StartTime = os:timestamp(),
+    Response = hackney:request(post, list_to_binary(URL), Headers, list_to_binary(Body), HackneyOptions),
+    Latency = timer:now_diff(os:timestamp(), StartTime),
+
+    case Response of
+        {ok, _, _, BodyRef} -> hackney:skip_body(BodyRef);
+        _ -> ok
+    end,
+
+    mzb_metrics:notify({"latency", histogram}, Latency),
+
+    case Response of
+        {ok, ExpectedCode, _, _} ->
+            mzb_metrics:notify({"http_ok", counter}, 1);
+        {ok, _, _, _} = Reply ->
+            lager:error("POST failed: ~p", [Reply]),
             mzb_metrics:notify({"http_fail", counter}, 1);
         E ->
             lager:error("hackney:request failed: ~p", [E]),
