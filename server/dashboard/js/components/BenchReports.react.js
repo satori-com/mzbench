589a23e075a9c646f8b4a70e9aebc894e15e8b02
Timofey Barmin <tbarmin@mz.com>
2017-06-01 17:48:10 -0700

    Reports page refactoring

--- server/dashboard/js/components/BenchReports.react.js
+++ server/dashboard/js/components/BenchReports.react.js
@@ -6,56 +6,19 @@ import PropTypes from 'prop-types';
 class BenchReports extends React.Component {
     constructor(props) {
         super(props);
-
-        this._openEmailReport = this._openEmailReport.bind(this);
-        this._onSendEmailReport = this._onSendEmailReport.bind(this);
-        this._onEmailChange = this._onEmailChange.bind(this);
-
-        this.state = {email: ""};
-    }
-
-    componentDidMount() {
-        $(ReactDOM.findDOMNode(this.refs.emailReportModal)).on('shown.bs.modal', () => {
-            ReactDOM.findDOMNode(this.refs.email).focus();
-        });
-    }
-
-    renderNoReportAvailable() {
-        const link = `#/bench/${this.props.bench.id}/logs`;
-        return (
-            <div className="alert alert-warning" role="alert">
-                Report is not available because this bench has not recordered any metrics. See <a href={link}>Logs</a> for the additional information.
-            </div>
-        );
     }
 
     renderDownloadReportPanel() {
-        const groups = this.props.bench.metrics.groups || [];
-
-        if (0 == groups.length) {
-            return this.renderNoReportAvailable();
-        }
-
         return (
             <div className="panel panel-default panel-report">
                 <div className="panel-heading">
-                    <h3 className="panel-title">Download report&nbsp;
-                    {this.props.bench.isRunning() ? <small>(email report is enabled for finished benches only)</small> : null}
-                    </h3>
+                    <h3 className="panel-title">Download report</h3>
                 </div>
-                <div className="panel-body">
-                    <button className="btn btn-primary" type="submit" disabled={this.props.bench.isRunning()} onClick={this._openEmailReport}>Email</button>
-                    <a href={`/data?id=${this.props.bench.id}`} target="_blank" className="btn btn-primary" type="submit">Text</a>
+                <div className="panel-body btn-toolbar">
+                    <a href={`/data?id=${this.props.bench.id}`} target="_blank" className="btn btn-primary" type="submit">Metrics</a>
+                    <a href={`/log?id=${this.props.bench.id}`} target="_blank" className="btn btn-primary" type="submit">System logs</a>
+                    <a href={`/userlog?id=${this.props.bench.id}`} target="_blank" className="btn btn-primary" type="submit">User logs</a>
                 </div>
-
-                <Modal ref="emailReportModal" onOk={this._onSendEmailReport} title="Send Email Report">
-                    <form onSubmit={this._onSendEmailReport}>
-                        <div className="form-group">
-                            <label className="control-label">Email:</label>
-                            <input ref="email" type="text" className="form-control" value={this.state.email} onChange={this._onEmailChange}/>
-                        </div>
-                    </form>
-                </Modal>
             </div>
         );
     }
@@ -66,32 +29,6 @@ class BenchReports extends React.Component {
                 {this.renderDownloadReportPanel()}
             </div>
         );
-    }
-
-    _onSendEmailReport(event) {
-        let notify = $.notify({message: `Sending report to ${this.state.email}... `}, {type: 'info', delay: 0});
-        $.ajax({
-            url: `/email_report?id=${this.props.bench.id}&addr=${this.state.email}`,
-            success: () => {
-                notify.update({message: `Report has been sent to ${this.state.email}`, type: 'success'});
-                setTimeout(() => notify.close(), 5000);
-            },
-            error: () => {
-                notify.update({message: `Sending report failed`, type: 'danger'});
-                setTimeout(() => notify.close(), 5000);
-            },
-            beforeSend: (xhr) => { AuthStore.addCSRFToken(xhr) }
-        });
-        event.preventDefault();
-        this.refs.emailReportModal.close();
-    }
-
-    _openEmailReport() {
-        this.refs.emailReportModal.open();
-    }
-
-    _onEmailChange(event) {
-        this.setState({email: event.target.value});
     }
 };
 
