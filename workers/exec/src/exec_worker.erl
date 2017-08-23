ce40e0fb86ef292e7e3e89d19901816df7b24b86
Timofey Barmin <tbarmin@mz.com>
2017-08-23 10:46:36 -0700

    Support statsd in exec worker

--- workers/exec/src/exec_worker.erl
+++ workers/exec/src/exec_worker.erl
@@ -3,10 +3,19 @@
 -export([
     initial_state/0,
     metrics/0,
-    execute/3
+    execute/3,
+    statsd_opts/3,
+    declare_metric/6,
+    statsd/1
 ]).
 
-initial_state() -> "".
+-define(DEFAULT_STATSD_PORT, 8125).
+
+initial_state() ->
+    #{
+        statsd_spawned => false,
+        statsd_opts => []
+    }.
 
 metrics() ->
     [
@@ -20,7 +29,19 @@ metrics() ->
         ]}
     ].
 
-execute(State, Meta, Command) ->
+declare_metric(State, _Meta, Group, Title, Name, Type) ->
+    mzb_metrics:declare_metric(Group, Title, Name, Type, #{}),
+    {nil, State}.
+
+statsd_opts(State, _Meta, Opts) ->
+    {nil, State#{statsd_opts => Opts}}.
+
+execute(State = #{statsd_spawned := StatsdSpawned}, Meta, Command) ->
+    NewState =
+        case StatsdSpawned of
+            true -> State;
+            false -> start_statsd(State)
+        end,
     WorkerId = proplists:get_value(worker_id, Meta, undefined),
     lager:info("Executing ~p... on ~p~n", [Command, WorkerId]),
     TimeStart = os:timestamp(),
@@ -32,7 +53,7 @@ execute(State, Meta, Command) ->
     end,
     TimeFinish = os:timestamp(),
     mzb_metrics:notify({"latency", histogram}, timer:now_diff(TimeFinish, TimeStart)),
-    {nil, State}.
+    {nil, NewState}.
 
 run(Command, _Opts, WorkerId) ->
     {ok, Pid, _OsPid} = exec:run(Command, [monitor, stdout, stderr,
@@ -43,9 +64,61 @@ get_data(Port, WorkerId) -> get_data(Port, WorkerId, []).
 
 get_data(Port, WorkerId, _Buffer) ->
     receive
-        {Stream, _Pid, Bytes} when (Stream == stdout) or (Stream == stderr) -> 
+        {stdout, _Pid, Bytes} ->
             lager:info("Worker ~p output: ~s", [WorkerId, Bytes]),
+            get_data(Port, WorkerId, []);
+        {stderr, _Pid, Bytes} ->
+            lager:error("Worker ~p error: ~s", [WorkerId, Bytes]),
             get_data(Port, WorkerId, []);
         {'DOWN', _ , _, _, {exit_status, Code}} -> Code;
         {'DOWN', _ , _, _, normal} -> 0
     end.
+
+statsd(Opts) ->
+    Port =
+        case proplists:get_value(port, Opts) of
+            undefined -> ?DEFAULT_STATSD_PORT;
+            P -> P
+        end,
+    {ok, ListenSocket} = gen_udp:open(Port, [binary, {active, false}]),
+    accept(ListenSocket).
+
+accept(Socket) ->
+    inet:setopts(Socket, [{active, once}]),
+    receive
+        {udp, Socket, _Host, _Port, Binary} ->
+            parse(Binary),
+            accept(Socket)
+    end.
+
+parse(<<>>) -> ok;
+parse(Bin) ->
+    lists:map(fun report/1, binary:split(Bin, <<"\n">>, [global])).
+
+report(<<>>) -> ok;
+report(Bin) ->
+    case binary:split(Bin, [<<"|">>, <<":">>], [global]) of
+        [Metric, Value, Type|_] ->
+            MetricStr = binary_to_list(Metric),
+            {TypeAtom, ValueInt} =
+                case Type of
+                    <<"c">> -> {counter, parse_num(Value)};
+                    <<"g">> -> {gauge, parse_num(Value)};
+                    _ -> lager:info("Unknown type ~p", [Type]), gauge
+                end,
+            mzb_metrics:notify({MetricStr, TypeAtom}, ValueInt);
+        _ -> lager:info("Unknown format: ~s", [Bin])
+    end.
+
+parse_num(Bin) ->
+    try erlang:binary_to_integer(Bin) of
+        V -> V
+    catch
+        _:_ ->
+            erlang:round(erlang:binary_to_float(Bin))
+    end.
+
+start_statsd(State = #{statsd_opts := Opts}) ->
+    erlang:spawn(?MODULE, statsd, [Opts]),
+    State.
+
