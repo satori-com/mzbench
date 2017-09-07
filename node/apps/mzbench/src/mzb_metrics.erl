19733e8641b917c5bcc55f2f1fa7c320cb3a4923
Timofey Barmin <tbarmin@mz.com>
2017-09-07 12:05:03 -0700

    Make it possible for derived metric resolver to return undefined

--- node/apps/mzbench/src/mzb_metrics.erl
+++ node/apps/mzbench/src/mzb_metrics.erl
@@ -243,6 +243,7 @@ evaluate_derived_metrics(#s{metric_groups = MetricGroups} = State) ->
     lists:foreach(fun ({Name, derived, #{resolver:= Resolver, worker:= {Provider, Worker}} = Opts}) ->
         Args = mzb_bc:maps_get(resolver_args, Opts, []),
         try Provider:apply(Resolver, Args, Worker) of
+            undefined -> ok; % nothing was calculated
             Val -> global_set(Name, gauge, Val)
         catch
             _:Reason -> system_log:error("Failed to evaluate derived metrics:~nWorker: ~p~nFunction: ~p~nReason: ~p~nStacktrace: ~p~n", [Worker, Resolver, Reason, erlang:get_stacktrace()])
